4fa06f9ccebc30aa35c5e23f0f905dc145c27892
paul serafim <paul.serafim.3021@gmail.com>
2022-11-18 12:49:55 +1100

    footercode

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
             <h5 className='font-bold'>COMMUNITY</h5>
             <p>Accessibility</p>
             <p>This is not a real site</p>
-            <p>It's a pretty awesome clone</p>
+            <p>Its a pretty awesome clone</p>
             <p>Lifelike</p>
             <p>Careers</p>
         </div>
